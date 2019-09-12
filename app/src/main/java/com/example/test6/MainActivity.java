766970703509c5cfe0bf27d342fbdca162a972c1
jacob mbuco <mbucojacob@gmail.com>
2019-09-12 21:55:06 +0300

    adding all componets

---
+++ app/src/main/java/com/example/test6/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.test6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements TopSectionFragment.TopSectionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void createMeme(String top, String bottom) {
+        BottomPictureFragment bottomFragment = (BottomPictureFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        bottomFragment.setMemeText(top, bottom);
+    }
+}
